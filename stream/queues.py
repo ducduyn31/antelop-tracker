4351a18c11ea3e656e37c005783fd74026e580f6
Duy Nguyen <ducduy.ababat@gmail.com>
2021-10-01 14:16:47 +0700

    remove pickle

--- stream/queues.py
+++ stream/queues.py
@@ -1,7 +1,6 @@
 import os
 
 from core.faust_app import faust_app as app
-from core.pubsub import PubSub
 from core.tracking_process import TrackingProcess
 from stream.topics import human_detect_full_queue, recognized_queue
 
@@ -11,8 +10,8 @@ handlers = dict()
 @app.agent(human_detect_full_queue, concurrency=int(os.getenv('FAUST_CONCURRENCY')))
 async def on_human_detected(stream):
     async for event in stream:
-        source, order, timestamp = event.source, event.frame_order, event.timestamp
-        detections = event.detections
+        source, order, timestamp = event['source'], event['frame_order'], event['timestamp']
+        detections = event['detections']
 
         if source not in handlers or not handlers[source].is_alive():
             proc = TrackingProcess(source=source, redis_uri=os.getenv('REDIS_URI'))
--- stream/topics.py
+++ stream/topics.py
@@ -1,22 +1,8 @@
-from typing import Optional
-
 from core.faust_app import faust_app as app
-from faust import Record
-
-
-class HumanDetectionMessage(Record, serializer='pickle'):
-    frame_id: str
-    frame: bytes
-    detections: any
-    source: str
-    timestamp: int
-    frame_order: int
-    object_id: Optional[int]
-
 
 human_detect_full_queue = app.topic(
     'human_detect',
-    value_type=HumanDetectionMessage
+    value_serializer='json'
 )
 
 tracking_queue = app.topic(
